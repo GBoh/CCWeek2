faeec33c7d35a562deeb9a2549af2b7f410cd804
GBoh <greg@bohanon3d.com>
2015-08-25 16:35:21 -0500

    data grid

---
+++ day2/DataGrid/DataGrid/Controllers/MoviesController.cs
@@ -0,0 +1,20 @@
+﻿using DataGridMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DataGridMVC.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Index()
+        {
+            MovieIndexViewModel vm = new MovieIndexViewModel();
+
+            return View(vm);
+        }
+    }
+}
---
+++ day2/ShoppingList/ShoppingList/Helpers/HtmlExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ShoppingList.Helpers
+{
+    public static class HtmlExtensions
+    {
+        public static MvcHtmlString FormatCurrency(this HtmlHelper helper, decimal amount)
+        {
+            if (amount > 10000)
+            {
+                var tb = new TagBuilder("span");
+                tb.MergeAttribute("style", "color:salmon");
+                tb.SetInnerText(amount.ToString("c"));
+                return MvcHtmlString.Create(tb.ToString(TagRenderMode.EndTag));
+            }
+
+            return MvcHtmlString.Create(amount.ToString("c"));
+        }
+    }
+}
