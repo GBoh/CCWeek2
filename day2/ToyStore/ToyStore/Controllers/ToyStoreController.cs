917ae10f8f8d3625377658dfed9b617534e0b5f0
GBoh <greg@bohanon3d.com>
2015-08-26 11:23:06 -0500

    Worlds best toy store Site

---
+++ day2/ToyStore/ToyStore/Controllers/ToyStoreController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ToyStore.Models;
+
+namespace ToyStore.Controllers
+{
+    public class ToyStoreController : Controller
+    {
+        // GET: ToyStore
+        public ActionResult Index()
+        {
+            List<Image> FeaturedImage = new List<Image>();
+            FeaturedImage.Add(new Image { ImageURL = "http://fun.familyeducation.com/images/Mr_Potato_Head_toy_H.jpg", ToyName = "Mr. Potato Head" });
+            FeaturedImage.Add(new Image { ImageURL = "https://www.bellalunatoys.com/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/h/a/haba-duck-wooden-pull-toys.jpg", ToyName = "Wooden Duck" });
+            FeaturedImage.Add(new Image { ImageURL = "http://fortrendymoms.files.wordpress.com/2012/11/fisher-price-laugh-and-learn.jpg", ToyName = "Puppy Dog" });
+
+            return View(FeaturedImage);
+        }
+    }
+}
---
+++ day2/ToyStore/ToyStore/Models/Image.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ToyStore.Models
+{
+    public class Image
+    {
+        public string ImageURL { get; set; }
+        public string ToyName { get; set; }
+
+    }
+}
